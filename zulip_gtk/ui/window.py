4c4d95de41cccf7390045b6b3ef9e187686cd492
Hannes <h@nnes.dev>
2022-01-19 17:50:42 +0000

    Move main paned to separate file

--- zulip_gtk/ui/window.py
+++ zulip_gtk/ui/window.py
@@ -6,6 +6,7 @@ from zulip_gtk.zulip_client import messages
 from zulip_gtk.zulip_client.streams import SubscriptionResponse
 from zulip_gtk.ui.stream import Stream
 from zulip_gtk.ui.header_bar import HeaderBar
+from zulip_gtk.ui.main_paned import MainPaned
 
 
 # Load Gtk
@@ -31,36 +32,3 @@ class Window(Gtk.ApplicationWindow):
         # Main
         self.main_paned = MainPaned()
         self.set_child(self.main_paned)
-
-
-class MainPaned(Gtk.Paned):
-    def __init__(self):
-        super().__init__()
-        self.set_orientation(Gtk.Orientation.HORIZONTAL)
-        self.set_position(200)  # default width
-
-        # Right
-        self.main_stack = self.MainStack()
-        self.set_end_child(self.main_stack)
-
-        # Left
-        self.sidebar = self.SideBar()
-        self.sidebar.set_stack(self.main_stack)
-        self.set_start_child(self.sidebar)
-
-    class MainStack(Gtk.Stack):
-        def __init__(self):
-            super().__init__()
-            self.set_transition_type(Gtk.StackTransitionType.SLIDE_RIGHT)
-
-        def add_stream(self, stream_data: SubscriptionResponse):
-            stream_widget = Stream(stream_data.name)
-            self.add_titled(
-                stream_widget,
-                name=stream_data.name,
-                title=stream_data.name,
-            )
-
-    class SideBar(Gtk.StackSidebar):
-        def __init__(self):
-            super().__init__()
