fd4a5785e203a18e14bf2719b62207c455b4af62
Hannes <h@nnes.dev>
2022-01-21 20:39:29 +0000

    Remove commented code

--- zulip_gtk/__main__.py
+++ zulip_gtk/__main__.py
@@ -10,7 +10,6 @@ def main():
     zulip_key = os.environ.get("ZULIP_KEY") or ""
     zulip_url = os.environ.get("ZULIP_URL") or ""
     zulip_client = ZulipClient(zulip_email, zulip_key, zulip_url)
-    # messages = zulip_client.get_messages("newest", 10, 0)
 
     app = ZulipGtk(zulip_client)
     exit_status = app.run(None)
